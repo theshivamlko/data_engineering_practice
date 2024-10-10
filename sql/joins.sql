c13c623de1d5518e88a1bf2ff7d1e632953aff36
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-10-10 17:40:30 +0530

    Update joins.sql
    CTE, Common Table Expressions

--- sql/joins.sql
+++ sql/joins.sql
@@ -4,6 +4,69 @@ JOIN order_items as oi
 ON o.order_id = oi.order_item_order_id
 
 
+select o.* ,oi.order_item_quantity
+from orders as o
+JOIN order_items as oi
+ON o.order_id = oi.order_item_order_id
+
+
+select count(DISTINCT order_id) from orders;
+select  DISTINCT order_item_order_id from order_items;
+
+select count(DISTINCT order_item_order_id) from order_items;
+
+
+select o.order_id , o.order_date,
+oi.order_item_id,
+oi.order_item_product_id,
+oi.order_item_subtotal 
+from orders as o
+LEFT OUTER JOIN order_items as oi
+ON o.order_id = oi.order_item_order_id order by order_id
+
+
+
+SELECT  o.order_date,
+        oi.order_item_product_id,
+	    round(sum(oi.order_item_subtotal)::numeric,2) as revenue
+FROM orders as o
+JOIN order_items as oi
+ON oi.order_item_order_id = o.order_id
+where o.order_status in ('COMPLETE','CLOSED') 
+Group by o.order_date,oi.order_item_product_id 
+order by o.order_date, revenue DESC;
+
+-- Views
+
+create or replace view order_details_view 
+as 
+select o.*, oi.order_item_product_id, oi.order_item_subtotal
+from orders as o 
+JOIN order_items as oi 
+ON o.order_id = oi.order_item_order_id
+
+select * from order_details_view
+
+create or replace view order_details_view 
+as 
+select o.*, oi.order_item_product_id, oi.order_item_subtotal, oi.order_item_quantity
+from orders as o 
+JOIN order_items as oi 
+ON o.order_id = oi.order_item_order_id
+
+
+-- CTE
+WITH order_details_cte
+as 
+(select o.*, oi.order_item_product_id, oi.order_item_subtotal, oi.order_item_quantity
+from orders as o 
+JOIN order_items as oi 
+ON o.order_id = oi.order_item_order_id )
+
+SELECT * from order_details_cte where order_status in ('CLOSED')
+
+
+
 
 
 
